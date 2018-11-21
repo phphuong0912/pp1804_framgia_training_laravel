2cd3c92084acec1ef77fc6a2871c5f346eb4c6a4
phphuong0912 <phphuong0912@gmail.com>
2018-11-21 20:56:24 +0700

    view base

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $faker->unique()->safeEmail
         'password' => $faker->password,
         'remember_token' => str_random(10),
     ];
