303ed6843fd69ad327920ab451c18ec3a4dc7ab5
phphuong0912 <phphuong0912@gmail.com>
2018-11-21 19:00:47 +0700

    migrate and seed

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -1,7 +1,5 @@
 <?php
-
 use Faker\Generator as Faker;
-
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -12,12 +10,9 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
-$factory->define(App\Models\User::class, function (Faker $faker) {
+$factory->define(App\Models\Ticket::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $faker->password,
-        'remember_token' => str_random(10),
+        'title' => $faker->word,
+        'content' => $faker->sentence
     ];
 });
